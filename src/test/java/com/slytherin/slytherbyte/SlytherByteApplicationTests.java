0ba4ed4fc79f466093058d482cf25f0cbfc04fbd
rengoering <regie.goering@gmail.com>
2025-06-06 15:55:56 +0200

    Initial commit

---
+++ src/test/java/com/slytherin/slytherbyte/SlytherByteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slytherin.slytherbyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlytherByteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
