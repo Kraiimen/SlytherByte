f483246ecaa5ae9853b1e4082985f7eccb4c3adb
Kraiimen <78323935+Kraiimen@users.noreply.github.com>
2025-06-14 13:35:58 +0200

    Corregge bug grafici e aggiunge warning per la sessione scaduta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,20 @@ import { Routes } from '@angular/router';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { AuthMainComponent } from './components/authentication/auth-main/auth-main.component';
 import { HomeComponent } from './components/home/home.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { ExpiredSessionComponent } from './components/expired-session/expired-session.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'auth', pathMatch: 'full' },
     { path: 'auth', component: AuthMainComponent },
-    { path: 'home', component: HomeComponent },
+    { 
+        path: 'app', 
+        component: MainContainerComponent, 
+        children: [
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'expired-session', component: ExpiredSessionComponent}
+        ]
+    },
     { path: 'user-profiles/:id', component: UserProfileComponent }
 ];
--- src/app/components/main-container/main-container.component.ts
+++ src/app/components/main-container/main-container.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { RouterModule } from '@angular/router';
+import { ExpiredSessionComponent } from "../expired-session/expired-session.component";
 
 @Component({
   selector: 'app-main-container',
-  imports: [],
+  imports: [NavBarComponent, RouterModule],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.css'
 })
