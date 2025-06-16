c4ace9cf33fa3cb4662d2040be528c3f7d60d69f
Kraiimen <78323935+Kraiimen@users.noreply.github.com>
2025-06-16 15:30:28 +0200

    Rimuove home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { AuthMainComponent } from './components/authentication/auth-main/auth-main.component';
-import { HomeComponent } from './components/home/home.component';
 import { CatalogueMainComponent } from './components/catalogue/catalogue-main/catalogue-main.component';
 import { MainContainerComponent } from './components/main-container/main-container.component';
 import { ExpiredSessionComponent } from './components/expired-session/expired-session.component';
@@ -9,14 +8,11 @@ import { ExpiredSessionComponent } from './components/expired-session/expired-se
 export const routes: Routes = [
     { path: '', redirectTo: 'auth', pathMatch: 'full' },
     { path: 'auth', component: AuthMainComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'user-profiles/:id', component: UserProfileComponent },
     { 
         path: 'app', 
         component: MainContainerComponent, 
         children: [
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
+            { path: '', redirectTo: 'catalogue', pathMatch: 'full' },
             { path: 'expired-session', component: ExpiredSessionComponent},
             { path: 'catalogue', component: CatalogueMainComponent }
         ]
